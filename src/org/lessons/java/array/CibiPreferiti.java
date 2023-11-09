12577607fffd46fd8e6b5011466196e5d0b33144
MichaelMantovani <michmantovani@gmail.com>
2023-11-09 17:24:42 +0100

    finish food snack

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -1,5 +1,20 @@
 package org.lessons.java.array;
 
 public class CibiPreferiti {
-
+public static void main(String[] args) {
+	String[] favFood = new String [7];
+	int ind = 0;
+	favFood[ind++] = "Lasagna";
+	favFood[ind++] = "Tortellini";
+	favFood[ind++] = "Carbonara";
+	favFood[ind++] = "Pizza";
+	favFood[ind++] = "Fiorentina";
+	favFood[ind++] = "Gelato";
+	favFood[ind++] = "Friggione";
+	
+	
+	System.out.println("La classifca conta " + favFood.length + " piatti");
+	System.out.println("Il mio cibo preferito è: " + favFood[0]);
+	System.out.println("Il mio cibo preferito ma non troppo è: " + favFood[favFood.length - 1]);
+}
 }

cf7b68f680666f3c85d07a8405ce405961b85afd
MichaelMantovani <michmantovani@gmail.com>
2023-11-09 17:43:02 +0100

    print median favourite food

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -2,7 +2,7 @@ package org.lessons.java.array;
 
 public class CibiPreferiti {
 public static void main(String[] args) {
-	String[] favFood = new String [7];
+	String[] favFood = new String [8];
 	int ind = 0;
 	favFood[ind++] = "Lasagna";
 	favFood[ind++] = "Tortellini";
@@ -10,11 +10,22 @@ public static void main(String[] args) {
 	favFood[ind++] = "Pizza";
 	favFood[ind++] = "Fiorentina";
 	favFood[ind++] = "Gelato";
+	favFood[ind++] = "Pollo";
 	favFood[ind++] = "Friggione";
 	
 	
 	System.out.println("La classifca conta " + favFood.length + " piatti");
 	System.out.println("Il mio cibo preferito è: " + favFood[0]);
 	System.out.println("Il mio cibo preferito ma non troppo è: " + favFood[favFood.length - 1]);
+	
+	
+	if (favFood.length % 2 == 0) {
+		String median1 = favFood[favFood.length / 2];
+		String median2 = favFood[favFood.length / 2 - 1];
+		System.out.println("I piatti di mezza classifica sono: " + median1 + " e " + median2);
+	} else {
+		String median1 = favFood[favFood.length / 2];
+		System.out.println("Il piatto di mezza classifica è: " + median1);
+	}
 }
 }

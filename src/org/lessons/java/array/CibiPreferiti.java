d8c5fee04419a77967b5e3e4e85abd474ada7ce4
MichaelMantovani <michmantovani@gmail.com>
2023-11-09 17:55:32 +0100

    add comments

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -2,6 +2,8 @@ package org.lessons.java.array;
 
 public class CibiPreferiti {
 public static void main(String[] args) {
+	
+//	Dichiaro all'array e lo popolo
 	String[] favFood = new String [7];
 	int ind = 0;
 	favFood[ind++] = "Lasagna";
@@ -12,12 +14,12 @@ public static void main(String[] args) {
 	favFood[ind++] = "Gelato";
 	favFood[ind++] = "Pollo";
 	
-	
+//	Stampo il numero di piatti nell'array ed il cibo in prima ed ultima posizione
 	System.out.println("La classifca conta " + favFood.length + " piatti");
 	System.out.println("Il mio cibo preferito è: " + favFood[0]);
 	System.out.println("Il mio cibo preferito ma non troppo è: " + favFood[favFood.length - 1]);
 	
-	
+//	Controllo,anche se non necessario, se la lunghezza dell'array è pari o dispari e stampo la mediana
 	if (favFood.length % 2 == 0) {
 		String median1 = favFood[favFood.length / 2];
 		String median2 = favFood[favFood.length / 2 - 1];
